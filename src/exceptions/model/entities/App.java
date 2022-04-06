b8ac160cc23b584e4d737f6279fb2e7122bcfed1
PedroF3rreira <pedro-edu19@outlook.com>
2022-04-05 22:15:21 -0300

    implementação inicial

---
+++ src/exceptions/model/entities/App.java
@@ -0,0 +1,37 @@
+package exceptions.model.entities;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) throws ParseException {
+		
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		System.out.print("Digite o numero do quarto: ");
+		int roomNumber = sc.nextInt();
+		
+		System.out.print("Digite a data de entrada(dd/mm/yyyy): ");
+		Date checkin = sdf.parse(sc.next()); 
+		
+		System.out.print("Digite a data de saída: (dd/mm/yyyy)");
+		Date checkout = sdf.parse(sc.next());
+		
+		Reservation rev = null;
+		
+		if(checkout.after(checkin)) {
+			 rev = new Reservation(roomNumber, checkin, checkout);
+			 System.out.println(rev);
+		}
+		else {
+			System.out.println("Erro data de saida e menor que data de entrada");
+		}
+		
+		sc.close();	
+	}
+
+}
---
+++ src/exceptions/model/entities/Reservation.java
@@ -0,0 +1,60 @@
+package exceptions.model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+	
+	private int roomNumber;
+	private Date checkin;
+	private Date checkout;
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
+	public Reservation(int roomNumber, Date checkin, Date checkout) {
+	
+		this.roomNumber = roomNumber;
+		this.checkin = checkin;
+		this.checkout = checkout;
+	}
+	
+	public Reservation() {
+		
+	}
+	
+	public long duration() {
+		long diff = checkout.getTime() - checkin.getTime();
+		
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+	}
+
+	public void updateDates(Date checkin, Date checkout) {
+		this.checkin = checkin;
+		this.checkout = checkout;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Reservation: RoomNumber " + roomNumber + " Checkin " + sdf.format(checkin) + " Checkout " + sdf.format(checkout) + " Days: " + this.duration();
+	}
+
+	public int getRoomNumber() {
+		return roomNumber;
+	}
+
+	public void setRoomNumber(int roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+	public Date getCheckin() {
+		return checkin;
+	}
+
+	public Date getCheckout() {
+		return checkout;
+	}
+
+	
+}
